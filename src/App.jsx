71b237e7709309cc42f4617bca0c9bf06279671d
RouisIbra <123466397+RouisIbra@users.noreply.github.com>
2023-02-05 15:09:45 +0100

    Persist theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-import {} from "@mui/material/styles";
 import HomePage from "./pages/HomePage";
 import ThemeMode from "./context/ThemeMode";
 import { ThemeProvider } from "@emotion/react";
-import { dark } from "@mui/material/styles/createPalette";
 import darkTheme from "./styles/darkTheme";
 import lightTheme from "./styles/lightTheme";
 
@@ -13,8 +11,20 @@ function App() {
   const [themeMode, setThemeMode] = useState("dark");
 
   const toggleTheme = useCallback(() => {
+    // toggle current theme state
     setThemeMode((prevState) => (prevState === "dark" ? "light" : "dark"));
-  });
+
+    // toggle theme mode from localStorage
+    localStorage.setItem("theme", themeMode === "dark" ? "light" : "dark");
+  }, [themeMode]);
+
+  // Get theme mode from persistent local storage
+  useEffect(() => {
+    const userThemeModePref = localStorage.getItem("theme");
+    if (userThemeModePref && themeMode !== userThemeModePref) {
+      setThemeMode(userThemeModePref === "dark" ? "dark" : "light");
+    }
+  }, []);
 
   return (
     <ThemeMode.Provider value={{ mode: themeMode, toggleTheme: toggleTheme }}>
